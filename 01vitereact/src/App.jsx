79a39d8abdd03ad7f01da71e538dc05a551395fd
Rojan7 <rojadhikari02@gmail.com>
2024-06-20 13:59:04 +0545

    committbeforelunch

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,19 @@
+
+ import Chai from "./chai"
+function App() {
+  
+
+  return (
+    <>
+      <Chai/>
+      <h1>chai aur react
+      </h1>
+      <p>test paraaaa</p>
+    </>
+    
+    
+     
+  )
+}
+
+export default App
