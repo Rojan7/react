1c4cc48acf2d246300094e5bda64a270d7fed342
Rojan7 <rojanadhikari02@gmail.com>
2024-09-17 15:17:02 +0000

    counterupdatewithreset

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -4,26 +4,36 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  
+let [counter,setCounter]=useState(0) ;
 
-  const addValue = () => {
-    if (counter < 20) {
-      setCounter(counter + 1);
-    }
-  };
-  const removeValue = () => {
-    if (counter > 0) {
-      setCounter(counter - 1);
-    }
-  };
+ const addValue =  () =>{
+  if (counter <20)
+  {
+    setCounter(counter +1);
+  }
+ }
+ const removeValue = ()=>{
+  if (counter >0)
+  {
+    setCounter (counter-1)
+  }
+ }
+ const resetValue = ()=>{
+  setCounter (counter -counter);
+ }
 
   return (
     <>
-      <h1>Chai aur react</h1>
-      <h2>Counter value :{counter}</h2>
-      <button onClick={addValue}>Add value</button>
+      <h1>Counter {counter}</h1>
+      <button onClick={addValue} > Add value</button>
+      <br />
       <br />
       <button onClick={removeValue}>Remove value</button>
+      <br />
+      <br />
+      <button onClick={resetValue}>Reset Value</button>
+
     </>
   );
 }
