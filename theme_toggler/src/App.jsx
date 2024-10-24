d66c2e4ca05f956f859a1d39e3df776e630a16eb
Rojan7 <rojanadhikari02@gmail.com>
2024-10-24 15:57:58 +0000

    bug clusture remove

--- theme_toggler/src/App.jsx
+++ theme_toggler/src/App.jsx
@@ -1,38 +1,16 @@
-import { useEffect, useState } from "react";
-import { ThemeProvider } from "./Context/Theme";
-import ThemeBtn from "./Component/Theme_Btn";
-import Card from "./Component/Card";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [thememode, setthememode] = useState("light")
-
-  const light_theme = ()=>{
-    setthememode("light")
-  }
-  const dark_theme = ()=>{
-    setthememode("dark")
-  }
-  useEffect(() => {
-    document.querySelector('html').classList.remove("light","dark")
-    document.querySelector('html').classList.add (thememode)
-    
-  }, [thememode])
+  const [count, setCount] = useState(0)
 
   return (
     <>
-  <ThemeProvider value={{thememode,light_theme,dark_theme}}>
-      <div className="flex flex-wrap min-h-screen items-center">
-        <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            <ThemeBtn/>
-          </div>
-
-          <div className="w-full max-w-sm mx-auto"><Card/></div>
-        </div>
-      </div>
-      </ThemeProvider>
+      <h1 className='bg-black'>Tailwind test</h1>
     </>
-  );
+  )
 }
 
-export default App;
+export default App 
